3ef9b6c64b9027b97bf47524bed69a952078ddaf
Nilabhro <nilabhro@tutamail.com>
2026-08-08 16:02:50 +0530

    Fix Vulkan platform redefinition

--- include/whis/vulkan.h
+++ include/whis/vulkan.h
@@ -1,14 +1,6 @@
 #ifndef WHIS_VULKAN_H_
 #define WHIS_VULKAN_H_ 1
 
-#if defined(WHIS_PLATFORM_X11)
-#define VK_USE_PLATFORM_XCB_KHR
-#elif defined(WHIS_PLATFORM_WAYLAND)
-#define VK_USE_PLATFORM_WAYLAND_KHR
-#elif defined(WHIS_PLATFORM_WIN32)
-#define VK_USE_PLATFORM_WIN32_KHR
-#endif /* WHIS_PLATFORM_* */
-
 #include "attr.h"
 #include "types.h"
 
